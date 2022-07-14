6df54f86869a1ee5f840ce8e0005b6c4b1c2d8b6
yhirayama202207 <108814571+yhirayama202207@users.noreply.github.com>
2022-07-14 06:52:18 +0000

    [Finish]Ruby-7

---
+++ lesson7.rb
@@ -0,0 +1,28 @@
+#upcase（小文字を大文字に変換）
+puts "webcamp" .upcase
+
+
+#FYI
+#upcase!（小文字を大文字に変換／破壊的メゾッド）
+puts "WebCamp" .upcase!
+
+#downcase（大文字を小文字に変換）
+puts "WEBCAMP" .downcase
+
+#downcase（大文字を小文字に変換／破壊的メゾッド）
+puts "WebCamp" .downcase!
+
+#swapcase（小文字を大文字に、大文字を小文字に）
+puts "WebCamp" .swapcase
+
+#swapcase!（小文字を大文字に、大文字を小文字に／破壊的メゾッド）
+puts "WebCamp" .swapcase!
+
+#capitalize（先頭の小文字を大文字に変換）
+puts "webcamp" .capitalize
+
+#capitalize!（先頭の小文字を大文字に変換／破壊的メゾッド）
+puts "webcamp" .capitalize!
+
+
+
